7b89ad5d545001905fdea9848600f1f8354de1aa
Kenneth <kennethkjliang@gmail.com>
2020-06-22 14:40:58 -0700

    edit username to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, :password_digest, :session_token, presence: true
-    validates :username, :session_token, uniqueness: true
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, :session_token, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true }
 
     after_initialize :ensure_session_token
@@ -11,8 +11,8 @@ class User < ApplicationRecord
 
     # spire
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
         user&.is_password?(password) ? user : nil
     end
 
