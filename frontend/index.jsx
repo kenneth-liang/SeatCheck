60aea031f5ead3c1282fb1dbe43a172b60dbe1ee
Kenneth <kennethkjliang@gmail.com>
2020-06-22 17:18:24 -0700

    added configureStore and Root, also added test

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
+import { signup, login, logout } from './util/session_api_util';
+
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore()
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to SeatCheck</h1>, root);
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+    ReactDOM.render(<Root store={store} />, root);
 });
+
