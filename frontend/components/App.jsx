f09ab086ac797c71742071e2894ec30d6e04c4a9
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:51:47 -0700

    aded Modal, AuthRoutes no longer needed

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import {Route} from 'react-router-dom';
+
+import GreetingContainer from "./greeting/greeting_container"
+import SignInFormContainer from './session_form/signin_form_container'
+import SignUpFormContainer from './session_form/signup_form_container'
+import Modal from "./modal/modal";
+
+import {AuthRoute} from '../util/route_util'
+
 
 const App = () => (
   <div>
-    <h1>SeatCheck</h1>
-    <h3>Where Your Seat Is Always Saved</h3>
+    <Modal/>
+    <header>
+      <h1>SeatCheck</h1>
+      <h3>Where Your Seat Is Always Saved</h3>
+      <GreetingContainer/>
+    </header>
   </div>
 );
 
 export default App;
+
+   
