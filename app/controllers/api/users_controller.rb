ba7366079c621636b85967342ffeda07474a9e2e
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:49:08 -0700

    commented debugger for testing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ class Api::UsersController < ApplicationController
             login(@user)
             render "api/users/show"
         else
-            debugger
+            # debugger
             render json: @user.errors.full_messages, status: 422
         end
     end
