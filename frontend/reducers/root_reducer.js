a14f7ae68a6a9e46d2a98fb2ea8a364928a8ac30
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:53:33 -0700

    added ui and errors reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
+import uiReducer from './ui_reducer'
 
 const rootRuducer = combineReducers({
     entities: entitiesReducer,
     session: sessionReducer,
-    errors: errorsReducer
+    errors: errorsReducer,
+    ui: uiReducer
 })
 export default rootRuducer;
