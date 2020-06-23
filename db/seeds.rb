716763707c6656c6adcfe6e05427252c4a715963
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:50:57 -0700

    added user seeds and demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+user1 = User.create!(
+    first_name: "Bobby",
+    last_name: "Lee",
+    email: "bobbyl@aa.io",
+    password: "password"
+)
+user2 = User.create!(
+    first_name: "Theo",
+    last_name: "Von",
+    email: "theov@aa.io",
+    password: "password"
+)
+user3 = User.create!(
+    first_name: "Andrew",
+    last_name: "Santino",
+    email: "andrews@aa.io",
+    password: "password"
+)
+
+demo = User.create!(
+    first_name: "Demo",
+    last_name: "User",
+    email: 'demo@aa.io',
+    password: 'password'
+)
