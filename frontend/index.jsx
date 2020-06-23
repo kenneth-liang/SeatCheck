173d7f2de355e57778ecf8175608574c9cd620bb
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:53:01 -0700

    boot strap current user

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -12,11 +12,29 @@ window.logout = logout;
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore()
+    // const store = configureStore()
     const root = document.getElementById("root");
 
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+            users: { [window.currentUser.id]: window.currentUser },
+            },
+            session: { id: window.currentUser.id },
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    //test
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
 
     ReactDOM.render(<Root store={store} />, root);
 });
