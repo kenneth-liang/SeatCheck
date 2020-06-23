511b4d328661bb504df77c14e7d4682fb1e03e48
Kenneth <kennethkjliang@gmail.com>
2020-06-23 13:48:42 -0700

    debugger for test

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
     def create
         @user = User.new(user_params)
         # debugger
@@ -6,6 +11,7 @@ class Api::UsersController < ApplicationController
             login(@user)
             render "api/users/show"
         else
+            debugger
             render json: @user.errors.full_messages, status: 422
         end
     end
