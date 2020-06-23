dd74ef18a11fa1a2b01dfa9c19a2532598ff218e
Kenneth <kennethkjliang@gmail.com>
2020-06-22 17:16:20 -0700

    cleaned up code for clarity

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
-        user&.is_password?(password) ? user : nil
+        user && user.is_password?(password) ? user : nil
     end
 
     def password=(password)
@@ -27,8 +27,8 @@ class User < ApplicationRecord
 
     def reset_session_token!
         self.session_token = SecureRandom.urlsafe_base64
-        save!
-        session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
