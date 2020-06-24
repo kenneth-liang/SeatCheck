2a9b7801185e6de99ff5d55f886dbc1b92543cd8
Kenneth <kennethkjliang@gmail.com>
2020-06-24 00:16:22 -0700

    NavBar added

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 import GreetingContainer from "./greeting/greeting_container"
 import SignInFormContainer from './session_form/signin_form_container'
 import SignUpFormContainer from './session_form/signup_form_container'
+
 import Modal from "./modal/modal";
+import NavBar from './nav_bar/nav_bar_container'
 
 import {AuthRoute} from '../util/route_util'
 
@@ -12,10 +14,14 @@ import {AuthRoute} from '../util/route_util'
 const App = () => (
   <div>
     <Modal/>
+    <NavBar />
     <header>
-      <h1>SeatCheck</h1>
-      <h3>Where Your Seat Is Always Saved</h3>
-      <GreetingContainer/>
+      {/* <Link to="/" className="header-link">
+        <h1>SeatCheck</h1>
+        {/* <h3>Where Your Seat Is Always Saved</h3> */}
+      {/* </Link>  */}
+   
+      {/* <GreetingContainer/> */}
     </header>
   </div>
 );
