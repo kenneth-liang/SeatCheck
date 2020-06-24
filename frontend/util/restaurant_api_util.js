f570cd80fde3259ad7bbfa3baba91d493a71c525
Kenneth <kennethkjliang@gmail.com>
2020-06-24 15:27:02 -0700

    restaurant api util

---
+++ frontend/util/restaurant_api_util.js
@@ -0,0 +1,14 @@
+export const fetchRestaurants = () => {
+    return  $.ajax({
+        method: "GET",
+        url: "/api/restaurants",
+    })
+}
+   
+
+export const fetchRestaurant = (id) => {
+    return $.ajax({
+        method: "GET",
+        url: `/api/restaurants/${id}`, 
+    })
+}
