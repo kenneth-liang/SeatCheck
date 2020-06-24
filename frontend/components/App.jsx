3cbbca734e6c924b1d33513370a94d20e49c5eb8
Kenneth <kennethkjliang@gmail.com>
2020-06-24 15:11:36 -0700

    added restaurant container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,31 +1,28 @@
 import React from "react";
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 
 import GreetingContainer from "./greeting/greeting_container"
 import SignInFormContainer from './session_form/signin_form_container'
 import SignUpFormContainer from './session_form/signup_form_container'
+import UserContainer from './user/user_container'
+import RestaurantIndexContainer from './restaurants/restaurant_index_container'
 
 import Modal from "./modal/modal";
 import NavBar from './nav_bar/nav_bar_container'
 
-import {AuthRoute} from '../util/route_util'
+import {AuthRoute, ProtectedRoute} from '../util/route_util'
 
 
 const App = () => (
   <div>
-    <Modal/>
+    <Modal />
     <NavBar />
-    <header>
-      {/* <Link to="/" className="header-link">
-        <h1>SeatCheck</h1>
-        {/* <h3>Where Your Seat Is Always Saved</h3> */}
-      {/* </Link>  */}
-   
-      {/* <GreetingContainer/> */}
-    </header>
+    <div>
+      <RestaurantIndexContainer/>
+    </div>
   </div>
 );
 
 export default App;
 
-   
+     
