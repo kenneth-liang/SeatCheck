862401ee5c826c3462e4713dac1ee24233f591d1
Kenneth <kennethkjliang@gmail.com>
2020-06-24 15:18:15 -0700

    added restaurants reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import usersReducer from "./user_reducer";
+import restaurantsReducer from './restaurants_reducer'
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    restaurants: restaurantsReducer
 })
 
 export default entitiesReducer;
