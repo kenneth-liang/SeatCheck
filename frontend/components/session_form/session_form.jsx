79679e111f04e64837dd94a76d6c04efa929c1ce
Kenneth <kennethkjliang@gmail.com>
2020-06-24 18:03:43 -0700

    fixed x button

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -55,8 +55,7 @@ class SessionForm extends React.Component{
         const displayForm = (this.props.formType === 'Create Account') ? (
             <div className="form-container">
                 <form onSubmit={this.handleSubmit} className="form-box">
-                    <div onClick={(e) => {e.stopPropagation
-                    this.props.closeModal;}} className="close-x">X</div>
+                    <div onClick={this.props.closeModal} className="close-x">X</div>
                     <h2 className="form-title">Welcome to SeatCheck!</h2>
                     {this.renderErrors()}
                     <input type="text" value={this.state.first_name} placeholder="First Name *" onChange={this.update("first_name")} className="form-input" />
