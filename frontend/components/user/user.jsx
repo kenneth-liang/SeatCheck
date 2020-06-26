a19927933471ef75b35c72f2dce023cf84c6f973
Kenneth <kennethkjliang@gmail.com>
2020-06-25 23:35:03 -0700

    completed user page

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 
 
 class UserProfile extends React.Component{
@@ -12,11 +12,49 @@ class UserProfile extends React.Component{
     }
 
     render () {
-        return(
-            <div>
-                <h1>{`${this.props.user.first_name}'s reservations & favorites:`}</h1>    
+        return (
+          <div className="page-container">
+            <div className="page-header">
+              <div className="page-header-content">
+                <h1 className="full-name">{`${this.props.user.first_name} ${this.props.user.last_name}`}</h1>
+              </div>
             </div>
-        )
+
+            <div className="content-group">
+              {/* <div className="page-nav-wraper"> */}
+              <nav className="page-nav">
+                <ul>
+                  <li className="page-links">
+                    <Link to="/api/reservations">Reservations</Link>
+                  </li>
+                  <li className="page-links">
+                    <Link to="/api/favorites/">Saved Restaurants</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="page-main-content">
+              <div className="content-reservations">
+                <div className="content header">
+                  <h3>Reservations</h3>
+                </div>
+                <div className="content-feed">
+                  <div>Display Reservations here, hidden when none</div>
+                </div>
+              </div>
+              <div className="content-favorites">
+                <div className="content header">
+                  <h3>Saved Restaurants</h3>
+                </div>
+                <div className="content-feed">
+                  <div>Display Favorites here, hidden when none</div>
+                </div>
+              </div>
+              {/* </div> */}
+            </div>
+          </div>
+        );
     }
 
 }
