6a91056d025bbb69f95d185d34af5268932dd6f7
Kenneth <kennethkjliang@gmail.com>
2020-06-25 17:02:23 -0700

    added photo to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
     validates :name, :address, :city, :state, :zip, :price,:cuisine,:open_time,:close_time, :phone_number,:description, presence: true
+
+    has_one_attached :photo
 end
