e6e4706ba17c5f99fa9d170b80076ad3da66c1ff
Kenneth <kennethkjliang@gmail.com>
2020-06-26 20:48:46 -0700

    udpated index

--- frontend/components/search/restaurant_index.jsx
+++ frontend/components/search/restaurant_index.jsx
@@ -14,12 +14,13 @@ class RestaurantIndex extends React.Component {
 
     render() {
         //include filters 
+        const { restaurants } = this.props;
         return (
           <div className="restaurant-container">
             <h1 className="available">Available for dinner now</h1>
             <div className="restaurant-items">
-              {this.props.restaurants.map((restaurant) => (
-                <IndexItem key={restaurant.id} restaurant={restaurant} />
+              {restaurants.map((restaurant) => (
+                <IndexItem key={restaurant.id} restaurant={restaurant}/>
               ))}
             </div>
           </div>
