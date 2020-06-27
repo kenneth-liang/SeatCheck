25e3e954b33b7942e17c5b884b3b28b16add3518
Kenneth <kennethkjliang@gmail.com>
2020-06-26 20:49:35 -0700

    added profile image tag

--- frontend/components/search/restaurant_index_item.jsx
+++ frontend/components/search/restaurant_index_item.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-
+import {withRouter} from 'react-router-dom'
 class IndexItem extends React.Component{
     constructor(props){
         super(props)
+        this.handleClick = this.handleClick.bind(this)
     }
 
-
+    handleClick(){
+        const restaurantId = this.props.restaurant.id;
+        this.props.history.push(`/api/restaurants/${restaurantId}`)
+    }
 
     render (){
         const {restaurant} = this.props
-        const bImg = {
+        const pImg = {
             backgroundImage: `url(${restaurant.photoURL})`
         }
         return (
-          <div className="restaurant">
-            <div className="img" id="rest-img" style={bImg}></div>
+          <div className="restaurant" onClick={this.handleClick}>
+            <div className="img" id="rest-img" style={pImg}></div>
             <div className="restaurant-info">
               <div id="rest-name">{restaurant.name}</div>
               <div className="review">
@@ -31,4 +35,4 @@ class IndexItem extends React.Component{
     }
 }
 
-export default IndexItem
+export default withRouter(IndexItem)
