f27a551a59fc3194db67eae4a2e3ecd434faf0bf
Kenneth <kennethkjliang@gmail.com>
2020-06-28 01:50:15 -0700

    adjusted route paths

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,10 +5,10 @@ import GreetingContainer from "./greeting/greeting_container"
 import SignInFormContainer from './session_form/signin_form_container'
 import SignUpFormContainer from './session_form/signup_form_container'
 import UserContainer from './user/user_container'
-import RestaurantIndexContainer from './restaurants/restaurant_index_container'
 import SearchContainer from './search/search_container'
 import RestaurantShowContainer from './restaurants/restaurant_show_container'
 import HomeContainer from './home/home_container'
+import RestaurantIndexContainer from "./restaurants/restaurant_index_container"
 
 import Modal from "./modal/modal";
 import NavBar from './nav_bar/nav_bar_container'
@@ -21,12 +21,11 @@ const App = () => (
     <Modal />
     <NavBar />
     <Switch>
-      <Route exact path="/" component={HomeContainer}/>
-      <Route path="/api/restaurants/:restaurantId" component={RestaurantShowContainer}/>
-      <Route path="/api/users/:id" component={UserContainer}/>
+      <Route path="/restaurants/:restaurantId" component={RestaurantShowContainer} />
+      <Route path="/restaurants" component={RestaurantIndexContainer} />
+      <Route path="/users/:id" component={UserContainer} />
+      <Route exact path="/" component={HomeContainer} />
     </Switch>
-
-
   </div>
 );
 
