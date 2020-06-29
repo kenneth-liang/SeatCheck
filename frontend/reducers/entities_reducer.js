82075365e3220ab7a76032f7c330817254263fbe
Kenneth <kennethkjliang@gmail.com>
2020-06-29 10:17:10 -0700

    added reservations reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 
 import usersReducer from "./user_reducer";
 import restaurantsReducer from './restaurants_reducer'
+import reservationsReducer from './reservation_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    restaurants: restaurantsReducer
+    restaurants: restaurantsReducer,
+    reservations: reservationsReducer,
 })
 
 export default entitiesReducer;
