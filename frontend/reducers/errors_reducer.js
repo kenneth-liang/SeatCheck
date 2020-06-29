cf5ca7129796975263f3c76f3f172939ea5c5a24
Kenneth <kennethkjliang@gmail.com>
2020-06-29 10:17:57 -0700

    added reservations errors, search errors not needed

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import sessionErrorsReducer from "./session_errors_reducer";
+// import searchErrorsReducer from './search_errors_reducer'
+import reservationErrorsReducer from "./reservations_errors_reducer";
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
+  // search: searchErrorsReducer,
+  reservation: reservationErrorsReducer
 });
 
 export default errorsReducer;
