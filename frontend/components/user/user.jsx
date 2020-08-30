7a8665146b8af4b150264756750a5e82f839f7ad
Kenneth <kennethkjliang@gmail.com>
2020-08-30 01:37:48 -0700

    added basic favorites section

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -6,12 +6,46 @@ import ReservationIndex from '../reservation/reservation_index_container'
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
+
+    this.favoriteRestaurants = this.favoriteRestaurants.bind(this)
   }
 
   componentDidMount() {
     Promise.all([this.props.fetchUser(this.props.currentUser.id) 
         ,this.props.fetchUserReservations(this.props.currentUser.id)
         ,this.props.fetchRestaurants()])
+        ,this.props.requestUserFavorites(this.props.currentUser.id)
+  }
+
+  favoriteRestaurants() {
+    let favorites = this.props.favorites;
+    if (Object.keys(favorites).length === 0) {
+      return (
+        <div>
+          <p>No Favorites</p>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          {Object.values(favorites).map((favorite, i) => 
+            <section key={`favorite-${i}`} className="reservation-list">
+              <div className="restaurant-detail-container">
+                <Link to={`/restaurants/${favorite.restaurant.id}`} className="restaurant-name">
+                  {favorite.restaurant.name} 
+                </Link>
+                <div>
+                  Cuisine: {favorite.restaurnt.cuisine}
+                </div>
+                <div>
+                  Phone: {favorite.restaurnt.phone_number}
+                </div>
+              </div>
+            </section>
+          )}
+        </div>
+      )
+    }
   }
 
   render() {
@@ -59,7 +93,9 @@ class UserProfile extends React.Component {
               <h3>Saved Restaurants</h3>
             </div>
             <div className="content-feed">
-              <div></div>
+              <div>
+                {this.favoriteRestaurants()}
+              </div>
             </div>
           </div>
         </div>
