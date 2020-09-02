cedc8378c2f26e119231f19014487162623e15d3
Kenneth <kennethkjliang@gmail.com>
2020-09-01 20:46:26 -0700

    styling for restaurant index page, made fonts bigger and added icons

--- frontend/components/restaurants/restaurant_index_item.jsx
+++ frontend/components/restaurants/restaurant_index_item.jsx
@@ -19,11 +19,36 @@ class IndexItem extends React.Component{
     return avgRating
   }
 
+  getChairsScoreRestaurant() {
+    const restaurant = this.props.restaurant;
+    let sum = 0;
+    restaurant.score_arr.forEach((score) => sum += score)
+    let restaurantAvgRating;
+    if (sum === 0) {
+      restaurantAvgRating = 0
+    } else {
+      restaurantAvgRating = (Math.floor(sum / restaurant.score_arr.length * 10) / 10)
+    }
+
+    const restRatingArray = []
+
+    for (let i = 0; i < restaurantAvgRating; i++) {
+      restRatingArray.push(
+        <i key={i} className="fas fa-chair fa-lg chair-filled"></i>
+      )
+    }
+
+    return restRatingArray
+  }
+
   render (){
-      const restaurant = this.props.restaurant
-      const pImg = {
-          backgroundImage: `url(${restaurant.photo})`
-      }
+    const restaurant = this.props.restaurant
+    const pImg = {
+        backgroundImage: `url(${restaurant.photo})`
+    }
+    
+    const moneyCheck = restaurant.price > 30 ? "$$$ " : "$$ "
+
     const phoneIcon = <img className="phone-icon" src="https://seat-check-seeds.s3-us-west-1.amazonaws.com/phone.png" />
       return (
         <Link to={`/restaurants/${restaurant.id}`}>
@@ -31,15 +56,14 @@ class IndexItem extends React.Component{
             <div className="restaurant-content">
                 <div className="img" id="rest-img" style={pImg}></div>
                 <div className="restaurant-info">
-                  <div className="rest-name">{restaurant.name}</div>
+                <div className="rest-name">{restaurant.name}  </div>
                   <div className="rest-review">
-                    <span>Ratings: tbd </span>
-                    <span>({restaurant.countRating})</span>
+                  <span>Rating: {this.getAverageRating()} {this.getChairsScoreRestaurant()}</span>
                   </div>
                   <div className="rest-row">
-                      <span>${restaurant.price}</span>
-                      <span>{restaurant.cuisine}</span>
-                      <span>{restaurant.city}</span>
+                    <span>{moneyCheck}</span>
+                  <span><i className="fas fa-utensils"></i> {restaurant.cuisine}</span>
+                  <span><i className="fas fa-map-marker-alt location-idx"></i> {restaurant.city}</span>
                   </div>
                   <div className="rest-contact">{phoneIcon} {restaurant.phone_number}</div>
               </div>
