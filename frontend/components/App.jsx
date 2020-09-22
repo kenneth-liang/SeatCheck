34271428d292b125ca3dba0b0796f65c600710df
Kenneth <kennethkjliang@gmail.com>
2020-09-21 18:22:19 -0700

    added footer links

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import {Route, Link, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
-import GreetingContainer from "./greeting/greeting_container"
-import SignInFormContainer from './session_form/signin_form_container'
-import SignUpFormContainer from './session_form/signup_form_container'
+
 import UserContainer from './user/user_container'
-import SearchContainer from './search/search_container'
 import RestaurantShowContainer from './restaurants/restaurant_show_container'
 import HomeContainer from './home/home_container'
 import RestaurantIndexContainer from "./restaurants/restaurant_index_container"
@@ -32,6 +29,11 @@ const App = () => (
       <div className="footer-description">
         Inspired by: 
         <img className="footer-icon-ot" src="https://seat-check-seeds.s3-us-west-1.amazonaws.com/opentablelogo.png"/>
+      </div>
+      <div className="footer-links">
+        <a href="https://github.com/kenneth-liang" target="_blank"><i className="fab fa-github"></i><p> Github</p></a><br />
+        <a href="https://www.linkedin.com/in/insightfulkennethliang/" target="_blank"><i className="fab fa-linkedin"></i><p> LinkedIn</p></a><br />
+        <a href="https://kennethliang.com/" target="_blank"><i className="fas fa-user user-icon" target="_blank"></i><p> About Me</p></a>
       </div>
     </footer>
   </div>
