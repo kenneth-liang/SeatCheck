0da242e9ee81c8cd98eb6a867cc88f1448637c14
Kenneth <kennethkjliang@gmail.com>
2023-12-11 14:12:27 -0800

    deploy render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,8 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-# clean
-rm -rf public
-# build
-npm install && npm run build
-# migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
-# postbuild
-# cp -a frontend/build/. public/
